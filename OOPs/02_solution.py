629d05e3f86798116c4f0e5bfd5aa55ce0dbcc18
Harshit Singh <harshitsingh05893312@gmail.com>
2025-12-29 20:59:51 +0530

    Update

---
+++ OOPs/02_solution.py
@@ -0,0 +1,12 @@
+# Add a method to the car class that displays the full name of the car (brand and model)-
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model 
+        
+    def full_details(self):
+        return f"{self.brand}, {self.model}"    
+    
+my_car = Car("Toyota", "Corolla")
+print(my_car.full_details())
