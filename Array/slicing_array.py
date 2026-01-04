19bd63b4cd4a3fe2ecef17dae95d856d75536284
Harshit Singh <harshitsingh05893312@gmail.com>
2026-01-05 02:42:51 +0530

    update

--- Array/slicing_array.py
+++ Array/slicing_array.py
@@ -5,4 +5,12 @@ val = array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9])
 newArray = val[2: 6]
 
 for i in range(0, len(newArray)):
+    # slicing 3,4,5,6 from val arary and storing it in newArrray
     print(newArray[i], end=" ")
+
+
+# LETS REVERSE ALL THE ELEMENTS
+
+reversedArray = val[::-1]
+for i in range(len(reversedArray)):
+    print(reversedArray[i], end=" ")
