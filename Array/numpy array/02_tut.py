001be0aa60e6802cb2efb0b176d4652cba98f350
Harshit Singh <harshitsingh05893312@gmail.com>
2026-01-05 03:55:06 +0530

    updated

--- Array/numpy array/02_tut.py
+++ Array/numpy array/02_tut.py
@@ -4,14 +4,15 @@ val = linspace(10, 20, 5)
 
 for i in val:
     print(i, end=" ")
-
-    # it will divide 10 - 20 in 5 parts
+# it will divide 10 - 20 in 5 parts
 
 print('\n')
+
 val2 = arange(10, 20, 2)
 
 for x in val2:
     print(x, end=" ")
+# output : 10 12 14 16 18
 
 print('\n')
 
@@ -19,3 +20,4 @@ val3 = zeros(10)
 
 for y in val3:
     print(y, end=" ")
+# output : 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0
