40f53f1aeeae491b73f6b148cddda5511e29f685
Harshit Singh <harshitsingh05893312@gmail.com>
2026-01-05 03:19:21 +0530

    update

---
+++ Array/numpy array/01_tut.py
@@ -0,0 +1,8 @@
+from numpy import *
+
+val = array([1, 2, 3, 4, 5, 6.5, 7, 8])
+
+for i in val:
+    print(i, end=" ")
+
+# numpy allows us to create hetrogeneous array where we don't have to mention the type of numbver in it
