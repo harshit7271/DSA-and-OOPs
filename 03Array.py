7c1e897ba347d03ebe208783bf39c40ef73443a6
Harshit Singh <harshitsingh05893312@gmail.com>
2026-01-04 20:13:48 +0530

    update

--- 03Array.py
+++ 03Array.py
@@ -18,3 +18,25 @@ print("\n")
 val.reverse()
 for i in range(0, len(val)):
     print(val[i], end=" ")
+
+print("\n")
+
+val.insert(1, 50)
+val.append(100)
+val[3] = 150        # overwrite at 4th place digit
+for i in range(len(val)):
+    print(val[i], end=" ")
+
+
+print('\n')
+
+# Copying Array
+copyArray = array(val.typecode, (x for x in val))
+print(f"copied array = {copyArray}")
+
+print('\n')
+
+# another way
+array2 = array(val.typecode, (x for x in val))
+for i in range(len(val)):
+    print(array2[i], end=" ")
