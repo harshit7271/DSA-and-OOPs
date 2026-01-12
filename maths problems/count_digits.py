69e4d8edb95680c008273ae0955e4935b9b2c972
Harshit Singh <harshitsingh05893312@gmail.com>
2026-01-13 02:40:54 +0530

    Update

--- maths problems/count_digits.py
+++ maths problems/count_digits.py
@@ -4,3 +4,19 @@ def CountDigit(num):
 
 num = 788756764353
 print(CountDigit(num))
+# output : 12
+
+# Another way
+
+
+def CountNum(num1):
+    count = 0
+    while num1 > 0:
+        num1 = num1//10
+        count += 1
+    return count
+# Output : 5
+
+
+num1 = 21653
+print(CountNum(num1))
