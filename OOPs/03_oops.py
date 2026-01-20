622711c8aea56f3b55acbce5418adb8eade3c546
Harshit Singh <harshitsingh05893312@gmail.com>
2026-01-20 20:10:16 +0530

    Update

---
+++ OOPs/03_oops.py
@@ -0,0 +1,9 @@
+class employee():
+    def __init__(self):
+        self.id = 102
+        self.designation = "ML Engineer"
+        self.salary = 75000
+
+
+sam = employee()
+print(sam.designation)
--- maths problems/Reversing_number.py
+++ maths problems/Reversing_number.py
@@ -1,7 +1,7 @@
 def reverseNum(num):
     ans = 0
     while num > 0:
-        ans = ans*10 + num % 10
+        ans = ans*10 + num % 10    # remainder = num % 10
         num = num//10
     return ans
 
