e383926525fa1947bbdd5c63c09602237573f4bd
Harshit Singh <harshitsingh05893312@gmail.com>
2026-01-20 20:47:07 +0530

    Updated

--- OOPs/03_oops.py
+++ OOPs/03_oops.py
@@ -1,12 +1,19 @@
 # Initiate class
-class employee():
+class employee:
     # special method/magic method/dunder method - constructor
     def __init__(self):
         self.id = 102
         self.designation = "ML Engineer"
         self.salary = 75000
 
+    def travel(self, destination):
+        print(f"The employee is traveling to {destination}")
+
 
 # create an object instance of the class
 sam = employee()
+# printing the attribute
 print(sam.designation)
+
+# calling the method
+sam.travel("New Delhi")
