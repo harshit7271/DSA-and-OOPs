9495bc36fa02d0c819eb4ace36fc50d4fe88106b
Harshit Singh <harshitsingh05893312@gmail.com>
2026-01-20 20:58:16 +0530

    Updated

--- OOPs/03_oops.py
+++ OOPs/03_oops.py
@@ -2,11 +2,14 @@
 class employee:
     # special method/magic method/dunder method - constructor (constructor get called automatically as we assign value to the object)
     def __init__(self):
+        print("Started executing attributes/data")
         self.id = 102
         self.designation = "ML Engineer"
         self.salary = 75000
+        print("Attributes has been initiated successfully")
 
     def travel(self, destination):
+        print("This travel function was called manually")
         print(f"The employee is traveling to {destination}")
 
 
