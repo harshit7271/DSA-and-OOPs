fe5722af74d1003ad97f5616b421fd236339aa63
Harshit Singh <harshitsingh05893312@gmail.com>
2026-01-20 20:35:38 +0530

    update

--- OOPs/03_oops.py
+++ OOPs/03_oops.py
@@ -1,9 +1,12 @@
+# Initiate class
 class employee():
+    # special method/magic method/dunder method - constructor
     def __init__(self):
         self.id = 102
         self.designation = "ML Engineer"
         self.salary = 75000
 
 
+# create an object instance of the class
 sam = employee()
 print(sam.designation)
