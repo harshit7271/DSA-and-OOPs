fa69f7cf95fd7259730a5927fddbe681e4b48dce
Harshit Singh <harshitsingh05893312@gmail.com>
2026-01-21 22:44:52 +0530

    Updated

--- OOPs/Project/encapsulation.py
+++ OOPs/Project/encapsulation.py
@@ -1,11 +1,20 @@
 from main import chatbook
-
+"""
 user1 = chatbook()
-"""print(user1.__name)"""    # this will raise an AttributeError because __name is private (encapsulated)
+""""print(user1.__name)""""    # this will raise an AttributeError because __name is private (encapsulated)
 
 # accessing the private attribute using name mangling
 print(user1._chatbook__name)
 print(user1.id)
 
+user2 = chatbook()
+print(user2.id)
+"""
+
+# ------------encapsulation using getter and setter methods----------------
+user1 = chatbook()
+print(user1.id)
+
+chatbook.set_id(15)
 user2 = chatbook()
 print(user2.id)
